c9aef36ebd3a155f3b29436c6a71ef65d5d85de1
Austin  Burke <aburk3@gmail.com>
2018-09-28 19:05:47 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   array.each do |number|
-    new_array = []
-    new_array << number * number
+    squared = []
+    squared << number * number
     return new_array
   end
 end
